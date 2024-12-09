833ddba35f6323cb917d9ce8ec1994895f5b2373
fatmapostaci <fatmapostaci@gmail.com>
2024-12-09 14:37:21 +0300

    FileClass içeriğine comment yazıldı

--- src/FileClass.java
+++ src/FileClass.java
@@ -5,7 +5,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileClass {
-
+    /**
+     *verileri dosyadan satır satır okuyarak ArrayList<String></String> e atar
+     * @param path  dosya yolunu input olarak alır  ( string )
+     * @return list ArrayList olarak verileri return eder
+     */
     public static ArrayList<String> readFile(String path) {
         File file = new File(path);
        ArrayList<String> list = new ArrayList<>();
@@ -20,6 +24,13 @@ public class FileClass {
         }
         return list;
     }
+
+    /**
+     * Manav alışverişi yapıldıktan sonra fiş detayını saklamak için bir dosya üretiyor
+     * Dosya mevcut ise -> dosyanın içini temizliyor.
+     * @param path  dosya yolunu input olarak alır (string)
+     *
+     */
     public static void createFile(String path) {
         File file = new File((path));
         try {
@@ -33,6 +44,12 @@ public class FileClass {
             e.printStackTrace();
         }
     }
+
+    /**
+     * String olarak aldığı metni belirtilen dosyaya yeni satır olarak ekler
+     * @param path  dosya yolunu input olarak alır (string)
+     * @param s   Dosyaya yazılacak veriyi string olarak alır
+     */
     public static void writeFile(String path,String s) {
         try {
             FileWriter file = new FileWriter(path,true);
